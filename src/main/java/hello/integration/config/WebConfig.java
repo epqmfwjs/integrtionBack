34d03e5f4887f47568293c6a131f92693d870294
Choi <epqmfwjs@naver.com>
2024-11-15 16:33:53 +0900

    배포 테스트 2

--- src/main/java/hello/integration/config/WebConfig.java
+++ src/main/java/hello/integration/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // CORS 설정
         registry.addMapping("/**")  // API 엔드포인트에 대한 패턴
-                .allowedOrigins("http://localhost:3000")  // React 앱의 주소
+                //.allowedOrigins("http://localhost:3000")  // React 앱의 주소
+                .allowedOrigins("http://gogolckh.ddns.net:8010")  // React 앱의 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true)  // 인증 정보 허용
