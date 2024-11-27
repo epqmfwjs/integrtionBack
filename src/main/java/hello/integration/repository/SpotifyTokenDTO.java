c2c5a848da239010a97ab795b768e805afa927fe
Choi <epqmfwjs@naver.com>
2024-11-27 19:49:26 +0900

    spotify 토큰 관리 서버로 이전 별도로그인없이 사용가능 리프레쉬토큰설정 완료

---
+++ src/main/java/hello/integration/repository/SpotifyTokenDTO.java
@@ -0,0 +1,27 @@
+package hello.integration.repository;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SpotifyTokenDTO {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("expires_in")
+    private Long expiresIn;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+}
